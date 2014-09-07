8d4cdebcbd5d29c8d671be0d306da7922955a099
Michael Wright <wrigh517@gmail.com>
2014-09-07 01:32:19 -0400

    member show decent

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -33,9 +33,12 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fill => [200, 200]
+     process :resize_to_limit => [200, 200]
    end
 
+   version :large do
+     process :resize_to_fill => [400, 400]
+   end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
